0fae921c1870c6bfe07e8c229f93da5320fb769b
Iram Lopez <iram24y@gmail.com>
2023-05-30 21:52:05 -0600

    something

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 var speed = 200.0
 var gravity = 20.0
 var jump = 400
+var pressed = 2
+
+func _ready():
+	$Sword/CollisionShape2D.disabled = true
 
 func _physics_process(delta):
 	Move(delta)
@@ -18,25 +22,41 @@ func Move(delta):
 			velocity.x = clamp(speed, 100, speed)
 			$Sprite2D.flip_h = false
 			$Anim.play("Walk")
+			$Sword.position = Vector2(25, 0)
 		if (movement < 0.0):
 			velocity.x -= speed * delta
 			velocity.x = clamp(speed, -100, -speed)
 			$Sprite2D.flip_h = true
 			$Anim.play("Walk")
+			$Sword.position = Vector2(-25, 0)
 			
 	## Basic Idle logic
 	elif (movement == 0):
 		velocity.x = 0.0
 		$Anim.play("Idle")
+	if Input.is_action_just_pressed("ui_jump"):
+		pressed -= 1
+		print(pressed)
+	
 	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
 		Jump()
-	if velocity.y < 0 and !is_on_floor():
+	if Input.is_action_just_pressed("ui_jump") and !is_on_floor() && pressed >= 1:
+		Jump()	
+	if pressed <= 0:
+		velocity.y = velocity.y
+	if is_on_floor():
+		pressed = 2
+		
+	if velocity.y < 0 and !is_on_floor() and pressed >= 1:
 		$Anim.play("Jump")
 	elif velocity.y > 10 and !is_on_floor():
 		$Anim.play("Fall")
+		
+	if Input.is_action_just_pressed("ui_sword"):
+		Sword()
 
 func Jump():
 	velocity.y -= jump
 		
-		
-		
+func Sword():
+	$Anim.play("Sword")
